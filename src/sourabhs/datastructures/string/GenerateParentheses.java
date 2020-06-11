852d4bb77fd4f4e53c3e0c7c48813b5806a7b45d
Sourabh Satapathy <vickysourabh@gmail.com>
2020-06-10 21:52:01 -0400

    JuneUpdate

--- src/sourabhs/datastructures/string/GenerateParentheses.java
+++ src/sourabhs/datastructures/string/GenerateParentheses.java
@@ -23,50 +23,50 @@ import java.util.List;
  */
 public class GenerateParentheses {
 
-	public List<String> generateParenthesis(int n) {
-		List<String> list = new ArrayList<>();
-		backtrack(list, "", 0, 0, n);
-		return list;
-	}
+  public List<String> generateParenthesis(int n) {
+    List<String> list = new ArrayList<>();
+    backtrack(list, "", 0, 0, n);
+    return list;
+  }
 
-	private void backtrack(List<String> list, String str, int open, int close, int max){
+  private void backtrack(List<String> list, String str, int open, int close, int max){
 
-		if(str.length() == max*2){
-			list.add(str);
-			return;
-		}
+    if(str.length() == max*2){
+      list.add(str);
+      return;
+    }
 
-		if(open < max)
-			backtrack(list, str+"(", open+1, close, max);
-			if(close < open)
-				backtrack(list, str+")", open, close+1, max);
-	}
-	
-	public List<String> generateParenthesisIterative(int n) {
-		List<List<String>> lists = new ArrayList<>();
-		lists.add(Collections.singletonList(""));
+    if(open < max)
+      backtrack(list, str+"(", open+1, close, max);
+    if(close < open)
+      backtrack(list, str+")", open, close+1, max);
+  }
+  
+  public List<String> generateParenthesisIterative(int n) {
+    List<List<String>> lists = new ArrayList<>();
+    lists.add(Collections.singletonList(""));
 
-		for (int i = 1; i <= n; ++i) {
-			final List<String> list = new ArrayList<>();
+    for (int i = 1; i <= n; ++i) {
+      final List<String> list = new ArrayList<>();
 
-			for (int j = 0; j < i; ++j) {
-				for (final String first : lists.get(j)) {
-					for (final String second : lists.get(i - 1 - j)) {
-						list.add("(" + first + ")" + second);
-					}
-				}
-			}
-			lists.add(list);
-		}
-		return lists.get(lists.size() - 1);
-	}
+      for (int j = 0; j < i; ++j) {
+        for (final String first : lists.get(j)) {
+          for (final String second : lists.get(i - 1 - j)) {
+            list.add("(" + first + ")" + second);
+          }
+        }
+      }
+      lists.add(list);
+    }
+    return lists.get(lists.size() - 1);
+  }
 
-	public static void main(String[] args) {
-		GenerateParentheses obj = new GenerateParentheses();
-		List<String> results = obj.generateParenthesis(3);
-		results.forEach(p-> System.out.println(p));
-		System.out.println("New Input");
-		results = obj.generateParenthesisIterative(3);
-		results.forEach(p-> System.out.println(p));
-	}
+  public static void main(String[] args) {
+    GenerateParentheses obj = new GenerateParentheses();
+    List<String> results = obj.generateParenthesis(3);
+    results.forEach(p-> System.out.println(p));
+    System.out.println("New Input");
+    results = obj.generateParenthesisIterative(3);
+    results.forEach(p-> System.out.println(p));
+  }
 }
